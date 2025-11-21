75f12035759a05c37df5fba411212b51e652a0eb
MeivanK20 <mbaye.ivan@gmail.com>
2025-11-21 10:12:17 +0100

    Mise à jour du code ou correctifs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,13 +2,11 @@ import java.util.Properties
 import java.io.FileInputStream
 
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    id("com.android.library") // → library pour JitPack
+    kotlin("android")
     kotlin("plugin.serialization") version "1.9.22"
 }
 
-// Read local.properties
 val localProperties = Properties()
 val localPropertiesFile = rootProject.file("local.properties")
 if (localPropertiesFile.exists()) {
@@ -20,17 +18,22 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.myimmo2"
         minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        
         // Supabase credentials
-        buildConfigField("String", "SUPABASE_URL", localProperties.getProperty("SUPABASE_URL") ?: "https://qxjrylsttlwythcczxwp.supabase.co")
-        buildConfigField("String", "SUPABASE_ANON_KEY", localProperties.getProperty("SUPABASE_ANON_KEY") ?: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4anJ5bHN0dGx3eXRoY2N6eHdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwODg0MTAsImV4cCI6MjA3NzY2NDQxMH0.uEOGHdY5Ml1-wwNJflw_bHK9BYbeIJP2W4Oy0WNaWNk")
+        buildConfigField(
+            "String",
+            "SUPABASE_URL",
+            localProperties.getProperty("SUPABASE_URL") ?: "https://qxjrylsttlwythcczxwp.supabase.co"
+        )
+        buildConfigField(
+            "String",
+            "SUPABASE_ANON_KEY",
+            localProperties.getProperty("SUPABASE_ANON_KEY") ?: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4anJ5bHN0dGx3eXRoY2N6eHdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwODg0MTAsImV4cCI6MjA3NzY2NDQxMH0.uEOGHdY5Ml1-wwNJflw_bHK9BYbeIJP2W4Oy0WNaWNk"
+        )
     }
 
     buildTypes {
@@ -42,13 +45,16 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
+
     buildFeatures {
         compose = true
         buildConfig = true
@@ -64,8 +70,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("com.github.Meik20:my-immo-2:v1.0")
-
 
     // Supabase
     implementation(platform("io.github.jan-tennert.supabase:bom:3.2.6"))
@@ -74,7 +78,6 @@ dependencies {
     implementation("io.github.jan-tennert.supabase:storage-kt")
     implementation("io.github.jan-tennert.supabase:realtime-kt")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
